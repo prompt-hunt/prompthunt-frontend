218541d4f69ddab4ed4a885a56cc5fc33532ed48
mattiapomelli <mattiapomelli@gmail.com>
2023-07-08 22:44:20 +0200

    Add goerli address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,4 +1,10 @@
-import { gnosis, hardhat, polygonMumbai, scrollTestnet } from "wagmi/chains";
+import {
+  gnosis,
+  goerli,
+  hardhat,
+  polygonMumbai,
+  scrollTestnet,
+} from "wagmi/chains";
 
 import { ChainMap } from "./chains";
 
@@ -7,6 +13,7 @@ export const PROMPT_HUNT_ADDRESS: ChainMap = {
   [polygonMumbai.id]: "",
   [gnosis.id]: "0x1E21e9bB2bf3EB0d4b2cFbD1e8Cc0653F42B8532",
   [scrollTestnet.id]: "0xceC6C334EddF49c33A6AF66CB0F4883D5669210F",
+  [goerli.id]: "0x1AC58B79a4d888a298296E0e0b17629e48f6c555",
 };
 
 export const DEPLOYMENT_BLOCK: { [chainId: number]: number } = {
@@ -14,4 +21,5 @@ export const DEPLOYMENT_BLOCK: { [chainId: number]: number } = {
   [polygonMumbai.id]: 0,
   [gnosis.id]: 28850454,
   [scrollTestnet.id]: 3694091,
+  [goerli.id]: 9312851,
 };
