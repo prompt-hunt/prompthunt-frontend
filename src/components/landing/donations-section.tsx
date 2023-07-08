e34e5f133e87c63b22088e56333a5da1c8c71b26
stepome <stefanopomelli23@gmail.com>
2023-07-08 22:14:28 +0200

    Changed colors

--- src/components/landing/donations-section.tsx
+++ src/components/landing/donations-section.tsx
@@ -4,11 +4,11 @@ import { Button } from "@components/basic/button";
 
 export const DonationsSection = () => {
   return (
-    <section className="rounded-box flex flex-col items-center gap-6 bg-accent px-5 py-20">
+    <section className="rounded-box flex flex-col items-center gap-6 bg-accent px-5 py-20 text-secondary">
       <h3 className="max-w-[30rem] text-center text-3xl font-bold">
         Donate to support the best prompt creators
       </h3>
-      <p className="max-w-[28rem] text-center text-lg text-base-content-neutral">
+      <p className="max-w-[28rem] text-center text-lg text-base-content-neutral text-secondary">
         Our funding mechanism supports the prompt creators that are more
         appreciated and valuable to the community.
       </p>
--- src/components/landing/introduction-section.tsx
+++ src/components/landing/introduction-section.tsx
@@ -1,6 +1,6 @@
 export const IntroducationSecion = () => {
   return (
-    <section className="rounded-box flex flex-col items-center gap-10 bg-accent px-5 py-20 text-center text-base-content">
+    <section className="rounded-box flex flex-col items-center gap-10 bg-accent px-5 py-20 text-center text-secondary">
       <p className="max-w-[34rem] text-3xl">
         AI is now accessible to everyone, but finding the right prompts requires
         time and effort. <b>Collaboration</b> and <b>crowdsourcing</b> is the
