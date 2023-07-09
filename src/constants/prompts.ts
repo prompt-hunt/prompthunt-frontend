ffb576858dfeb6484580d71f845d6bff27634520
mattiapomelli <mattiapomelli@gmail.com>
2023-07-09 02:05:52 +0200

    Change categories

--- src/constants/prompts.ts
+++ src/constants/prompts.ts
@@ -1,7 +1,9 @@
 export const PROMPT_MODELS = ["GPT", "Stable Diffusion"];
 
 export const PROMPT_CATEGORIES = [
+  "Productivity",
+  "Writing",
+  "Business",
   "Art & Illustration",
-  "Productivity & Writing",
-  "Marketing & Business",
+  "Logos",
 ];
